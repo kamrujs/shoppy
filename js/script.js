4dbf9313862d23d77a59f84f66fcf64203589b88
kamrujs <source.akash@gmail.com>
2021-04-08 15:51:24 +0600

    res com

--- js/script.js
+++ js/script.js
@@ -14,8 +14,8 @@ $('.banner_slider').slick({
     responsive: [{
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 1,
+                slidesToScroll: 1,
                 infinite: true,
                 dots: true
             }
@@ -23,12 +23,12 @@ $('.banner_slider').slick({
         {
             breakpoint: 600,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
         },
         {
-            breakpoint: 480,
+            breakpoint: 576,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
